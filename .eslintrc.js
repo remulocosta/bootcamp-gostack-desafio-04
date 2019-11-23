dd71502a5ab1478e38afa57d0cf6dbfc5238d5b5
Remulo Costa <remulo.costa@gmail.com>
2019-11-23 02:11:40 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+      es6: true,
+      node: true,
+      browser: true,
+  },
+  extends: [
+      "airbnb-base",
+      "prettier",
+      "eslint:recommended",
+      "plugin:react/recommended",
+  ],
+  plugins: [
+      "prettier",
+      "react",
+  ],
+  globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly",
+      document: false
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module",
+      "ecmaFeatures": {
+        "jsx": true
+      },
+  },
+  rules: {
+      "prettier/prettier": "error",
+      "class-methods-use-this": "off",
+      "no-param-reassign": "off",
+      "camelcase": "off",
+      "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+  },
+};
